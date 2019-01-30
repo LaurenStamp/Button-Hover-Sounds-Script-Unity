998c0e186fbc80ca3ed2ffca9234fa2bb71eb8d4
LaurenStamp <47026726+LaurenStamp@users.noreply.github.com>
2019-01-31 00:13:36 +1100

    Button Hover/ Click
    Script for allowing sounds when hovering a mouse over a button or clicking it

---
+++ ButtonHover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class ButtonHover : MonoBehaviour, ISelectHandler , IPointerEnterHandler
+{
+    // This script gives the game more flavour by adding in audio sounds when mouse hovers over buttons and presses buttons
+
+	public AudioClip buttonhover;
+	public AudioClip buttonPress;
+	AudioSource audioSource;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+        // When mouse hovers over button play sound
+		audioSource.clip = buttonhover;
+		audioSource.Play();
+
+	}
+
+	public void OnSelect(BaseEventData eventData)
+	{
+        // On mouse click on button play sound
+		audioSource.clip = buttonPress;
+		audioSource.Play();
+	}
+}
